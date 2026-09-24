503173a59b3150e0ac8a3808d5f41d1f6157296a
Razdee01 <rahmanrajdee@gmail.com>
2026-09-24 20:16:21 +0600

    navbar created according to figma

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import logo from "@/assets/logo.png";
 
 const NavBar = () => {
+  const links = (
+    <>
+      <Link href="/Work-out">Workouts</Link>
+      <Link href="/My-plans">My Plans</Link>
+    </>
+  );
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-base-100 shadow-sm container mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -28,52 +37,20 @@ const NavBar = () => {
               tabIndex={-1}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <Link href="/" className="btn btn-ghost text-xl">
+            <Image src={logo} alt="logo" />
+            <p>FITLOG</p>
+          </Link>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+        <div className="navbar-center hidden lg:flex ">
+          <ul className="menu menu-horizontal px-1 gap-3">{links}</ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <Link href={"/Work-out"} className="btn">Plan</Link>
+          <Link href={"/Work-out"} className="btn">Saved</Link>
         </div>
       </div>
     </div>
